32f7c89d3d496039540c460a4ab31abc31aa6a35
Albin Rodriguez <ealbinu@gmail.com>
2022-04-27 13:43:35 -0500

    P1-10

--- assets/modulos/info.js
+++ assets/modulos/info.js
@@ -21,6 +21,7 @@ Vue.component('info', {
                     <lottie-player v-if="type=='relacionar'" src="../../assets/lottie/Relacionar.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop autoplay></lottie-player>
                     <lottie-player v-if="type=='input'" src="../../assets/lottie/Input.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop autoplay></lottie-player>
                     <lottie-player v-if="type=='keydown'" src="../../assets/lottie/Keydown.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop autoplay></lottie-player>
+                    <lottie-player v-if="type=='video'" src="../../assets/lottie/Video.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop autoplay></lottie-player>
                 </div>
             </div>
             <button class="reset button" @click="reset">Reiniciar</button>
